091e8deabe2562be9b94717c87663d43d60c7232
180046iwasaki <te180046@gmail.com>
2021-09-06 14:17:10 +0900

    bean作った

--- src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsRepository.java
+++ src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsRepository.java
@@ -3,6 +3,6 @@ package jp.jc21.r03te4a.teamdev.team2.ems.ems;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-public interface EmsRepository extends MongoRepository<設備管理Bean, Integer>{
+public interface EmsRepository extends MongoRepository<設備管理Bean, String>{
     
 }
