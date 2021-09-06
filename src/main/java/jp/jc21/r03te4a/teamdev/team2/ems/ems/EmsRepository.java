772c9b34347ca17b57d043f5e7b5278285562eeb
SeiyaIwabuchi <0123ook@gmail.com>
2021-09-06 13:15:07 +0900

    リポジトリ追加

---
+++ src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsRepository.java
@@ -0,0 +1,8 @@
+package jp.jc21.r03te4a.teamdev.team2.ems.ems;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface EmsRepository extends MongoRepository<設備管理Bean, Integer>{
+    
+}
