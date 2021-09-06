77f558730cf7f38f85a881e8895ae616964b635b
SeiyaIwabuchi <0123ook@gmail.com>
2021-09-06 13:14:57 +0900

    加藤君の代行でコントローラ追加

---
+++ src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsController.java
@@ -0,0 +1,27 @@
+package jp.jc21.r03te4a.teamdev.team2.ems.ems;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/list")
+public class EmsController {
+
+  // EmsRepositoryの実装
+  @Autowired
+  EmsRepository EmsRepository;
+
+  @GetMapping
+  String list(Model model) {
+    model.addAttribute("", EmsRepository.findAll());
+    return "/"; //TODO テンプレートファイルのパス
+  }
+
+}
