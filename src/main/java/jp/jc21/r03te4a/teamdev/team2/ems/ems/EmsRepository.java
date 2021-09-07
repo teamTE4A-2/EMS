3911e9cf360c9b7dc94eb9b3ef66a974272d86c7
SeiyaIwabuchi <0123ook@gmail.com>
2021-09-07 12:19:51 +0900

    クラス名の修正

--- src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsRepository.java
+++ src/main/java/jp/jc21/r03te4a/teamdev/team2/ems/ems/EmsRepository.java
@@ -1,8 +1,7 @@
 package jp.jc21.r03te4a.teamdev.team2.ems.ems;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
-public interface EmsRepository extends MongoRepository<設備管理Bean, String>{
+public interface EmsRepository extends MongoRepository<設備管理bean, String>{
     
 }
